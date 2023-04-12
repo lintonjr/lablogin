a7ce6dafce83d0005259caeb406cc6bac07f4666
Linton Junior <linton@motorola.com>
2023-04-12 14:21:41 -0400

    feat: firebase login

--- app/src/main/java/com/login/lablogin/Register.java
+++ app/src/main/java/com/login/lablogin/Register.java
@@ -1,33 +1,61 @@
 package com.login.lablogin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Register extends AppCompatActivity {
 
+//    https://firebase.google.com/docs/auth/android/password-auth
+//    FirebaseUser user = mAuth.getCurrentUser();
     EditText txtEmail, txtSenha;
     Button btnSubmit;
+    FirebaseAuth mAuth;
+    ProgressBar progressBar;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mAuth = FirebaseAuth.getInstance();
         setContentView(R.layout.activity_register);
         txtEmail = findViewById(R.id.email);
         txtSenha = findViewById(R.id.senha);
         btnSubmit = findViewById(R.id.btnSubmit);
+        progressBar = findViewById(R.id.progressbar);
+        textView = findViewById(R.id.toLogin);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
         btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                progressBar.setVisibility(View.VISIBLE);
                 String email, password;
                 email = String.valueOf(txtEmail.getText());
                 password = txtSenha.getText().toString();
@@ -42,7 +70,25 @@ public class Register extends AppCompatActivity {
                     return;
                 }
 
+                mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                progressBar.setVisibility(View.GONE);
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d("Auth Success", "createUserWithEmail:success");
+                                Toast.makeText(Register.this, "Account created.",
+                                        Toast.LENGTH_SHORT).show();
 
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w("Auth Failure", "createUserWithEmail:failure", task.getException());
+                                Toast.makeText(Register.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                });
             }
         });
     }
